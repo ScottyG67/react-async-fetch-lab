0f3eaab5cb217ca74a7a38d98aae0795992bd512
github username <scott.gloyna01@gmail.com>
2021-03-18 20:13:23 -0500

    commit tree a5d31cb65446b44b255f1afd75193a2199dd64d7

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+    state={
+        names: []
+    }
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json").then(res=>res.json()).then(response => {this.setState({names: response.people})})
+        // response => {this.setState({names: response.people})}
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
